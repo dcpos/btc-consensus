6f34b260c4f2b2602d1f7d8e172ca49a59c94c6a
Ian Coleman <coleman.ian@gmail.com>
2016-04-12 12:22:33 +1000

    blockchain.info source parses json before using it

--- src/sources/blockchain_dot_info.js
+++ src/sources/blockchain_dot_info.js
@@ -5,12 +5,13 @@ exports.BlockchainDotInfo = function() {
     this.address = function(addr, handler) {
         var url = "https://blockchain.info/address/" + addr + "?format=json";
         request(url, function(err, response, body) {
+            var data = JSON.parse(body);
             var result = {
                 address: addr,
-                balance: body.final_balance,
-                total_received_net: body.total_received,
-                total_sent_net: body.total_sent,
-                tx_count: body.n_tx,
+                balance: data.final_balance,
+                total_received_net: data.total_received,
+                total_sent_net: data.total_sent,
+                tx_count: data.n_tx,
                 txs: [], // TODO
                 utxos: [], // TODO
             };
