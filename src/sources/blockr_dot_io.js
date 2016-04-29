757d2916b8de73a3ac7ebcbd065392f4d0f52a72
Ian Coleman <coleman.ian@gmail.com>
2016-04-29 14:44:52 +1000

    BlockrDotIo.tx() implementation

--- src/sources/blockr_dot_io.js
+++ src/sources/blockr_dot_io.js
@@ -46,7 +46,7 @@ exports.BlockrDotIo = function() {
                 var utxo = {
                     tx_id: dataUtxo.tx,
                     tx_output: dataUtxo.n,
-                    amount: parseFloat(dataUtxo.amount) * 1e8,
+                    amount: toSatoshis(dataUtxo.amount),
                     confirmations: dataUtxo.confirmations,
                 };
                 utxos.push(utxo);
@@ -77,6 +77,53 @@ exports.BlockrDotIo = function() {
             var oldestFirst = txHashes.reverse();
             handler(null, oldestFirst);
         });
+    }
+
+    this.tx = function(txid, handler) {
+        var url = "https://btc.blockr.io/api/v1/tx/info/" + txid;
+        request.get({
+            url: url,
+            json: true,
+        }, function(err, response, data) {
+            if (err) {
+                handler(err);
+            }
+            if (response.statusCode != 200) {
+                handler("blockr_dot_io statusCode: " + response.statusCode);
+            }
+            var result = {
+                block_height: data.data.block,
+                block_time: new Date(data.data.time_utc).getTime() / 1000,
+                tx_id: data.data.tx,
+                fee: toSatoshis(data.data.fee),
+                inputs: [],
+                outputs: [],
+            }
+            // inputs
+            result.inputs = data.data.vins.map(function(input) {
+                return {
+                    address: input.address,
+                    txid: input.vout_tx,
+                    amount: toSatoshis(input.amount),
+                    tx_output: input.n,
+                }
+            });
+            // outputs
+            result.outputs = data.data.vouts.map(function(output) {
+                return {
+                    address: output.address,
+                    amount: toSatoshis(output.amount),
+                }
+            });
+            handler(null, result);
+        });
+    }
+
+    function toSatoshis(btcStr) {
+        var btc = parseFloat(btcStr);
+        var satoshis = Math.round(btc * 1e8);
+        var positive = Math.abs(satoshis);
+        return positive;
     }
 
 }
